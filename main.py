f44616f73b42509c77b05b3be19ea8c9069f9586
FuziCodes <100248571+Fuz1C@users.noreply.github.com>
2022-02-23 01:38:11 +0000

    Added code for if you are the only person eating the pizza.

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ sliceperson = slices // people
 remains = slices % people
 print (f"Each person gets {sliceperson}!")
 
-if remains <= 1:
+if people == 1:
+  print("Oh wait.. that 'each person' is you...")
+  print("...eating pizza by yourself?")
+  print("Why did you summon me then????")
+if remains == 1:
   print (f"There is {remains} slice remaining... FIGHT FOR THE DEATH FOR IT GOOOOO, THIS IS MY ONLY ENTERTAINMENT!!!!")
 if remains == 0:
   print(f"oh... there are no spare slices...")
-if people == 1:
-  print(f"Wow... eating pizza by yourself?")
 else:
   print(f"There are {remains} slices remaining... FIGHT FOR THE DEATH, I'LL GRANT YOU WEAPONS. GOOOOOOOOOOOOO, THIS IS MY ONLY ENTERTAINMENT!!!!!")
